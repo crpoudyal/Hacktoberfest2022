4695e746ac981b533ab295994f5081af12782d1e
unknown <imagekunwar@gmail.com>
2022-10-18 11:37:48 +0545

    feat: gRPC : Hello world

---
+++ mn/firstapplication/src/main/java/grpcdemo/GrpcServer.java
@@ -0,0 +1,17 @@
+package grpcdemo;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GrpcServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder
+                .forPort(8080)
+                .addService(new HelloServiceImpl()).build();
+
+        server.start();
+        server.awaitTermination();
+    }
+}
